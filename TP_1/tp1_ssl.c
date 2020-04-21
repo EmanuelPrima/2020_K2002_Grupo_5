543602bdf8346301435c410fb860392f19ceaad7
Facundo Pittaluga <fpittaluga@est.frba.utn.edu.ar>
2020-04-20 23:11:20 -0300

    Ultimate Version
    mirtha ya llego

--- TP_1/tp1_ssl.c
+++ TP_1/tp1_ssl.c
@@ -1,34 +1,8 @@
 #include <stdio.h>
 
-int stringLength (char* str)
-{
-	int i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return i;
-}
-
-void stringVaciar (char* str)
-{
-	int i = stringLength(str);
-	while (i>=0)
-	{
-		str[i] = '\0';
-		i--;
-	}
-}
 
 
-void addChar (char* str, char c)
-{
-	str[stringLength(str)] = c;
-	str[stringLength(str)+1] = '\0';
-}
-
-int verificarCaracter(char c)
-{
+int verificarCaracter(char c){
 	int i;
 	if (c == '0')
 		i = 0;
@@ -46,29 +20,19 @@ int verificarCaracter(char c)
 	
 }
 
-void actualizarEstado(int* estado, char c, int automata[7][5])
+int actualizarEstado(int estado, char c, int automata[7][5])
 {
 	int columna = verificarCaracter(c);
 	if (columna == -1)
-		*estado = 6;
+		return 6;
 	else
-		*estado = automata[*estado][columna];
+		return automata[estado][columna];
+		
 }
 
-
-int procesarCadena (char* str, int automata[7][5])
+int procesarCaracter (char c, int automata[7][5],int estado)
 {
-	int estado;
 	int res;
-	estado = 0;
-	char c;
-	int max = stringLength(str), i = 0;
-	while (i<max)
-	{
-		c = str[i];
-		actualizarEstado(&estado, c, automata);
-		i++;
-	}
 	if (estado == 1)
 		res = 0;
 	else if (estado == 2 || estado == 3)
@@ -80,41 +44,47 @@ int procesarCadena (char* str, int automata[7][5])
 	return res;
 }
 
+
 int main ()
 {
+	int estado = 0;
 	FILE* f;
 	FILE * m;
-	char str[100] = "";
 	char c;
 	int indice;
 	char resultado[4][20] = {{"decimal"}, {"octal"}, {"hexadecimal"}, {"desastre :("}};
-	int automata[7][5] = {{2, 1, 1, 6, 6},
-			      {1, 1, 1, 6, 6},
-			      {3, 3, 6, 4, 6},
-			      {3, 3, 6, 6, 6},
-			      {5, 5, 5, 6, 5},
-			      {5, 5, 5, 6, 5},
-			      {6, 6, 6, 6, 6}};
+	int automata[7][5] = {{2, 1, 1, 6, 6}, 
+	                    {1, 1, 1, 6, 6},
+			            {3, 3, 6, 4, 6},
+			            {3, 3, 6, 6, 6},
+			            {5, 5, 5, 6, 5},
+			            {5, 5, 5, 6, 5},
+			            {6, 6, 6, 6, 6}};
+			      
 	f = fopen("entrada.txt", "r");
+	
 	m = fopen("salida.txt", "w");
+	
 	fputs("BIENVENIDOS AL TP N°1 DE SINTAXIS Y SEMANTICA DE LOS LENGUAJES DEL GRUPO 5\n",m);
+	
 	while (!feof(f))
 	{
 		c = fgetc(f);
-		if (c == ',' || c == '\n')
-		{
-			indice = procesarCadena(str, automata);
-			fputs ("\n",m);
-			fputs(str, m);
-			fputs(" es un ", m);
-			fputs(resultado[indice],m);
-			stringVaciar(str);
+	
+	    if(c == ',' || c == EOF){
+	        
+			indice = procesarCaracter(c, automata,estado);
+			fputs (" es un ", m);
+			fputs(resultado[indice], m);
+			fputc('\n', m);
+			estado = 0;
 		}
-		else
-		{
-			addChar(str, c);
-		}
+		else {	
+			estado = actualizarEstado(estado,c,automata);
+			fputc(c,m);
+		} 
 	}
 	fclose(f);
+	fclose(m);
 	return 0;
 }
